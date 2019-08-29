a7d0fb505153de48fba189e1005c017dce5a0897
chenweize1998 <chenweize1998@users.noreply.github.com>
2019-08-30 00:03:42 +0800

    add log out, download & upload

--- NewsToday/app/src/main/java/com/example/newstoday/Activity/Table.java
+++ NewsToday/app/src/main/java/com/example/newstoday/Activity/Table.java
@@ -2,7 +2,6 @@ package com.example.newstoday.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.ActionBar;
 import android.app.SearchManager;
 import android.content.ComponentName;
 import android.content.Context;
@@ -10,19 +9,15 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
-import android.graphics.drawable.Drawable;
-import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
-import android.view.Gravity;
+import android.util.ArraySet;
 import android.view.View;
 import android.widget.ImageButton;
-import android.widget.LinearLayout;
 import android.widget.SearchView;
 import android.widget.Toast;
 
 import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.io.InputStream;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -40,18 +35,12 @@ import com.example.newstoday.R;
 import com.example.newstoday.UserManagerOnServer;
 import com.example.newstoday.AsyncServerNews;
 import com.example.newstoday.WechatShareManager;
-import com.mikepenz.iconics.IconicsDrawable;
-import com.mikepenz.iconics.typeface.IIcon;
 import com.mikepenz.materialdrawer.*;
-import com.mikepenz.materialdrawer.holder.ImageHolder;
-import com.mikepenz.materialdrawer.holder.StringHolder;
 import com.mikepenz.materialdrawer.model.*;
 import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
 
 import com.mikepenz.materialdrawer.model.interfaces.IProfile;
 import com.scwang.smartrefresh.layout.api.RefreshLayout;
-import com.scwang.smartrefresh.layout.footer.BallPulseFooter;
-import com.scwang.smartrefresh.layout.footer.FalsifyFooter;
 import com.scwang.smartrefresh.layout.listener.OnLoadMoreListener;
 import com.scwang.smartrefresh.layout.listener.OnRefreshListener;
 
@@ -71,19 +60,26 @@ public class Table extends AppCompatActivity {
 
     private Drawer drawer;
     private AccountHeader header;
+    private ArraySet<String> account = new ArraySet<>();
+    private int identifier = 3;
+    private int position = 0;
 
     private static final int DISMISS_TIMEOUT = 500;
 
     private final int CAT_REARRANGE = 1;
-    private final int USER_REQUEST = 2;
+    private final int LOGIN_REQUEST = 2;
     private final int PICK_IMAGE = 3;
+    private final int LOGOUT_REQUEST = 4;
 
     private final int COLLECTION_IDENTIFIER = 1;
     private final int HISTORY_IDENTIFIER = 2;
     private final int CLEAR_IDENTIFIER = 3;
     private final int NIGHT_IDENTIFIER = 4;
-    private final int LOGIN_IDENTIFIER = 5;
-    private final int LOGOUT_IDENTIFIER = 6;
+    private final int UPLOAD_IDENTIFIER = 5;
+    private final int DOWNLOAD_IDENTIFIER = 6;
+
+    private final int LOGIN_IDENTIFIER = 1;
+    private final int LOGOUT_IDENTIFIER = 2;
 
 
 
@@ -98,111 +94,7 @@ public class Table extends AppCompatActivity {
         /**
          * Drawer
          */
-        BaseDrawerItem item1 = new PrimaryDrawerItem().withIdentifier(COLLECTION_IDENTIFIER).withName("我的收藏")
-                .withIcon(R.drawable.star).withTextColor(Color.parseColor("#ababab"));
-        BaseDrawerItem item2 = new SecondaryDrawerItem().withIdentifier(HISTORY_IDENTIFIER).withName("浏览历史")
-                .withIcon(R.drawable.history).withTextColor(Color.parseColor("#ababab"));
-        BaseDrawerItem item3 = new SecondaryDrawerItem().withIdentifier(CLEAR_IDENTIFIER).withName("清除历史")
-                .withIcon(R.drawable.clear).withTextColor(Color.parseColor("#ababab"));
-        SwitchDrawerItem switchDrawerItem = new SwitchDrawerItem().withIdentifier(NIGHT_IDENTIFIER).withName("夜间模式")
-                .withIcon(R.drawable.night).withTextColor(Color.parseColor("#ababab")).withSelectable(false);
-        header = new AccountHeaderBuilder()
-                .withActivity(this)
-                .addProfiles(
-                        new ProfileDrawerItem().withName("Weize Chen").withIdentifier(1)
-                        .withEmail("wei10@mails.tsinghua.edu.cn").withIcon(R.drawable.chenweize),
-                        new ProfileDrawerItem().withName("Hao Peng").withIdentifier(2)
-                                .withEmail("h-peng17@mails.tsinghua.edu.cn").withIcon(R.drawable.penghao)
-                )
-                .addProfiles(
-                        new ProfileSettingDrawerItem().withName("Add Account")
-                                .withIcon(R.drawable.plus).withIdentifier(LOGIN_IDENTIFIER),
-                        new ProfileSettingDrawerItem().withName("Manage Your Account")
-                                .withIcon(R.drawable.setting).withIdentifier(LOGOUT_IDENTIFIER))
-                .withOnAccountHeaderListener(new AccountHeader.OnAccountHeaderListener() {
-                    @Override
-                    public boolean onProfileChanged(View view, IProfile profile, boolean current) {
-                        if (profile.getIdentifier() == LOGIN_IDENTIFIER){
-                            Intent intent = new Intent(getApplicationContext(), Login.class);
-                            startActivityForResult(intent, USER_REQUEST);
-                        }
-                        return false;
-                    }
-                })
-                .withOnAccountHeaderProfileImageListener(new AccountHeader.OnAccountHeaderProfileImageListener() {
-                    @Override
-                    public boolean onProfileImageClick(View view, IProfile profile, boolean current) {
-                        if(header.getActiveProfile() == profile) {
-                            Intent intent = new Intent();
-                            intent.setType("image/*");
-                            intent.setAction(Intent.ACTION_GET_CONTENT);
-                            startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE);
-                        }
-                        header.setActiveProfile(profile.getIdentifier(), true);
-                        return false;
-                    }
-
-                    @Override
-                    public boolean onProfileImageLongClick(View view, IProfile profile, boolean current) {
-                        return false;
-                    }
-                })
-                .withTextColor(Color.parseColor("#ababab"))
-                .build();
-        drawer = new DrawerBuilder()
-                .withActivity(this)
-                .withAccountHeader(header)
-                .withSelectedItem(-1)
-                .addDrawerItems(
-                        item1,
-                        item2,
-                        new DividerDrawerItem(),
-                        item3,
-                        switchDrawerItem
-                )
-                .withSliderBackgroundDrawableRes(R.drawable.drawer_bg)
-                .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
-                    @Override
-                    public boolean onItemClick(View view, int position, IDrawerItem drawerItem){
-                        // do something with the clicked item :D
-                        if(drawerItem.getIdentifier() == COLLECTION_IDENTIFIER) {
-                            Intent intent = new Intent(getApplicationContext(), CollectionNews.class);
-                            startActivity(intent);
-
-//                            startActivityForResult(intent, COLLECTION_CHANGED);
-//                            if(userManagerOnServer.userSignUp("h-peng17", "123456")){
-//                                System.out.println("用户注册成功");
-//                            }else{
-//                                System.out.println("用户注册失败");
-//                            }
-                        } else if (drawerItem.getIdentifier() == HISTORY_IDENTIFIER) {
-                            Intent intent = new Intent(getApplicationContext(), HistoryNews.class);
-                            startActivity(intent);
-//                            startActivityForResult(intent, HISTORY_CHANGED);
-//                            if(userManagerOnServer.userSignOut()){
-//                                System.out.println("用户退出成功");
-//                            }else{
-//                                System.out.println("用户退出失败");
-////                            }
-//                            if(userManagerOnServer.userSignIn("h-peng17","123456")){
-//                                System.out.println("用户登录成功");
-//                            }else{
-//                                System.out.println("用户登陆失败");
-//                            }
-//                            if(asyncServerNews.asyncHistoryNewsFromServer()){
-//                                System.out.println("同步成功");
-//                            }else{
-//                                System.out.println("同步失败");
-//                            }
-                        } else if (drawerItem.getIdentifier() == CLEAR_IDENTIFIER) {
-                            newsManager.deleteAllHistory();
-                            mAdapterNews.notifyDataSetChanged();
-                            Toast.makeText(getApplicationContext(), "历史记录已清除", Toast.LENGTH_LONG).show();
-                        }
-                        return false;
-                    }
-                })
-                .build();
+        buildDrawer();
 
         /**
          * Search view
@@ -397,16 +289,27 @@ public class Table extends AppCompatActivity {
                 mAdapterNews.updateNews(news);
                 mAdapterNews.notifyDataSetChanged();
             }
-        } else if(requestCode == USER_REQUEST){
+        } else if(requestCode == LOGIN_REQUEST){
             if(resultCode == RESULT_OK){
                 String email = (String) data.getSerializableExtra("email");
+                if(account.contains(email)){
+                    Toast.makeText(getApplicationContext(), "账号已登陆", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                account.add(email);
                 String name = (String) data.getSerializableExtra("name");
-                header.addProfiles(new ProfileDrawerItem().withName(name)
-                        .withEmail(email).withIdentifier(header.getProfiles().size()));
-                header.setActiveProfile(header.getProfiles().get(header.getProfiles().size() - 1), true);
+                header.addProfile(new ProfileDrawerItem().withName(name)
+                        .withEmail(email).withIdentifier(identifier)
+                        .withIcon(R.drawable.header), position);
+                header.setActiveProfile(identifier, true);
+                header.updateProfile(header.getProfiles().get(identifier - 1));
+                ++identifier;
+                ++position;
             }
         } else if(requestCode == PICK_IMAGE){
             try {
+                if(data == null)
+                    return;
                 InputStream inputStream = getContentResolver().openInputStream(data.getData());
                 Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
                 header.getActiveProfile().withIcon(bitmap);
@@ -415,8 +318,138 @@ public class Table extends AppCompatActivity {
                 e.printStackTrace();
                 System.exit(-1);                // 调试完以后注释掉！！！！！！！！！！！！！！！！！！！！
             }
-
         }
+    }
+
+    private void buildDrawer(){
+        BaseDrawerItem item1 = new PrimaryDrawerItem().withIdentifier(COLLECTION_IDENTIFIER).withName("我的收藏")
+                .withIcon(R.drawable.ic_star).withTextColor(Color.parseColor("#ababab"));
+        BaseDrawerItem item2 = new SecondaryDrawerItem().withIdentifier(HISTORY_IDENTIFIER).withName("浏览历史")
+                .withIcon(R.drawable.ic_history).withTextColor(Color.parseColor("#ababab"));
+        BaseDrawerItem item3 = new SecondaryDrawerItem().withIdentifier(UPLOAD_IDENTIFIER).withName("上传用户数据")
+                .withIcon(R.drawable.ic_cloud_upload).withTextColor(Color.parseColor("#ababab"));
+        BaseDrawerItem item4 = new SecondaryDrawerItem().withIdentifier(DOWNLOAD_IDENTIFIER).withName("下载用户数据")
+                .withIcon(R.drawable.ic_cloud_download).withTextColor(Color.parseColor("#ababab"));
+        BaseDrawerItem item5 = new SecondaryDrawerItem().withIdentifier(CLEAR_IDENTIFIER).withName("清除历史")
+                .withIcon(R.drawable.clear).withTextColor(Color.parseColor("#ababab"));
+        SwitchDrawerItem switchDrawerItem = new SwitchDrawerItem().withIdentifier(NIGHT_IDENTIFIER).withName("夜间模式")
+                .withIcon(R.drawable.night).withTextColor(Color.parseColor("#ababab")).withSelectable(false);
+        header = new AccountHeaderBuilder()
+                .withActivity(this)
+                .addProfiles(
+//                        new ProfileDrawerItem().withName("Weize Chen").withIdentifier(3)
+//                        .withEmail("chenweize@mails.tsinghua.edu.cn").withIcon(R.drawable.chenweize),
+//                        new ProfileDrawerItem().withName("Hao Peng").withIdentifier(4)
+//                                .withEmail("h-peng17@mails.tsinghua.edu.cn").withIcon(R.drawable.penghao)
+                )
+                .addProfiles(
+                        new ProfileSettingDrawerItem().withName("Add Account")
+                                .withIcon(R.drawable.plus).withIdentifier(LOGIN_IDENTIFIER),
+                        new ProfileSettingDrawerItem().withName("Log Out")
+                                .withIcon(R.drawable.ic_exit_to_app).withIdentifier(LOGOUT_IDENTIFIER))
+                .withOnAccountHeaderListener(new AccountHeader.OnAccountHeaderListener() {
+                    @Override
+                    public boolean onProfileChanged(View view, IProfile profile, boolean current) {
+                        if (profile.getIdentifier() == LOGIN_IDENTIFIER){
+                            Intent intent = new Intent(getApplicationContext(), Login.class);
+                            startActivityForResult(intent, LOGIN_REQUEST);
+                        } else if (profile.getIdentifier() == LOGOUT_IDENTIFIER){
+                            if(identifier == 3) {
+                                Toast.makeText(getApplicationContext(), "当前没有用户登录", Toast.LENGTH_SHORT).show();
+                                return false;
+                            }
+                            header.removeProfileByIdentifier(header.getActiveProfile().getIdentifier());
+                            --identifier;
+                            --position;
+                            if(identifier != 3)
+                                header.setActiveProfile(identifier - 1);
+                        }
+                        return false;
+                    }
+                })
+                .withOnAccountHeaderProfileImageListener(new AccountHeader.OnAccountHeaderProfileImageListener() {
+                    @Override
+                    public boolean onProfileImageClick(View view, IProfile profile, boolean current) {
+                        if(header.getActiveProfile() == profile) {
+                            Intent intent = new Intent();
+                            intent.setType("image/*");
+                            intent.setAction(Intent.ACTION_GET_CONTENT);
+                            startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE);
+                        }
+                        header.setActiveProfile(profile.getIdentifier(), true);
+                        return true;
+                    }
+
+                    @Override
+                    public boolean onProfileImageLongClick(View view, IProfile profile, boolean current) {
+                        return false;
+                    }
+                })
+                .withTextColor(Color.parseColor("#ababab"))
+                .build();
+        drawer = new DrawerBuilder()
+                .withActivity(this)
+                .withAccountHeader(header)
+                .withSelectedItem(-1)
+                .addDrawerItems(
+                        item1,
+                        item2,
+                        new DividerDrawerItem(),
+                        item3,
+                        item4,
+                        item5,
+                        new DividerDrawerItem(),
+                        switchDrawerItem
+                )
+                .withSliderBackgroundDrawableRes(R.drawable.drawer_bg)
+                .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
+                    @Override
+                    public boolean onItemClick(View view, int position, IDrawerItem drawerItem){
+                        // do something with the clicked item :D
+                        if(drawerItem.getIdentifier() == COLLECTION_IDENTIFIER) {
+                            Intent intent = new Intent(getApplicationContext(), CollectionNews.class);
+                            startActivity(intent);
+
+//                            startActivityForResult(intent, COLLECTION_CHANGED);
+//                            if(userManagerOnServer.userSignUp("h-peng17", "123456")){
+//                                System.out.println("用户注册成功");
+//                            }else{
+//                                System.out.println("用户注册失败");
+//                            }
+                        } else if (drawerItem.getIdentifier() == HISTORY_IDENTIFIER) {
+                            Intent intent = new Intent(getApplicationContext(), HistoryNews.class);
+                            startActivity(intent);
+//                            startActivityForResult(intent, HISTORY_CHANGED);
+//                            if(userManagerOnServer.userSignOut()){
+//                                System.out.println("用户退出成功");
+//                            }else{
+//                                System.out.println("用户退出失败");
+////                            }
+//                            if(userManagerOnServer.userSignIn("h-peng17","123456")){
+//                                System.out.println("用户登录成功");
+//                            }else{
+//                                System.out.println("用户登陆失败");
+//                            }
+//                            if(asyncServerNews.asyncHistoryNewsFromServer()){
+//                                System.out.println("同步成功");
+//                            }else{
+//                                System.out.println("同步失败");
+//                            }
+                        } else if (drawerItem.getIdentifier() == CLEAR_IDENTIFIER) {
+                            newsManager.deleteAllHistory();
+                            mAdapterNews.notifyDataSetChanged();
+                            Toast.makeText(getApplicationContext(), "历史记录已清除", Toast.LENGTH_LONG).show();
+                        } else if (drawerItem.getIdentifier() == UPLOAD_IDENTIFIER) {
+                            newsManager.deleteAllHistory();
+                            asyncServerNews.asyncHistoryNewsFromServer();
+                        } else if (drawerItem.getIdentifier() == DOWNLOAD_IDENTIFIER) {
+                            asyncServerNews.asyncCollectionNewsToServer();
+                            asyncServerNews.asyncHistoryNewsToServer();
+                        }
+                        return false;
+                    }
+                })
+                .build();
     }
 
     @Override
